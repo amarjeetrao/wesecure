ab955347ebd13e75a881079a9bb3d0ecfae930f9
amarjeetrao <amarjeet@traidweb.in>
2020-10-02 14:29:02 +0530

    responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 (function () {
-	var swiper = new Swiper('.swiper-container', {
+	// testimonial slider
+	let swiper = new Swiper('.swiper-container', {
 		autoHeight: true,
 		autoplay: {
 			delay: 4500,
@@ -9,5 +10,15 @@
 			el: '.swiper-pagination',
 			clickable: true
 		}
+	});
+
+	//navigation mobile menu
+	let menuToggle = document.querySelector('.navbar-toggler');
+
+	menuToggle.addEventListener('click', function () {
+		mobileTarget = this.dataset.target;
+
+		let mobileMenu = document.querySelector(mobileTarget);
+		mobileMenu.classList.toggle('slided');
 	});
 })();
