daa0e787339afe115fe1121b8f62d3a00b7cda3b
amarjeetrao <amarjeet@traidweb.in>
2020-10-02 10:11:39 +0530

    footer and testimonal added

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+(function () {
+	var swiper = new Swiper('.swiper-container', {
+		autoHeight: true,
+		autoplay: {
+			delay: 4500,
+			disableOnInteraction: false
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true
+		}
+	});
+})();
